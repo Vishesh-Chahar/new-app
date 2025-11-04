08b17df73dd3ad9d55ee6a2f06f193f956a5e4bc
Vishesh-Chahar <vishesh.chahar01@gmail.com>
2025-11-04 13:08:59 +0530

    all2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// replace `your-repo-name` below with your GitHub repo name
+// Replace `new-app` with your actual repo name
 export default defineConfig({
-  base: "/new-app/",
-  plugins: [react()],
-});
+    plugins: [react()],
+    base: '/new-app/',
+})
